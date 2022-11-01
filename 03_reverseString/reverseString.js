7ac8475c0da57a2359d8c0f44ff3cbfba1ade1e8
Lugdentube <samhayes1020@gmail.com>
2022-11-01 12:31:23 -0700

    completed exercises 1-7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+  let tempArray = string.split('');
+  tempArray.reverse();
+  let reversed = tempArray.join('');
+  return reversed;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...manyMoreArgs) {
+  for (let i = 0; i < manyMoreArgs.length; i++) {
+    let index = array.indexOf(manyMoreArgs[i]);
+    if (index > -1) array.splice(index,1);
+  }
+  return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
